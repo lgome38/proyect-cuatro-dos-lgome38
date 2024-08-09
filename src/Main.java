001ca2812ea35f3bc94280f5184150ac23ead1bf
Gómez Barrera <LGOME38@bancodebogota.com.co>
2024-08-09 12:22:03 -0500

    taller-cuatro-dos

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int sumatoria=0;
+        for (int s = 1; s <= 10 ; s++){
+            sumatoria += s;
+            System.out.println("total de la suma es :" + sumatoria);
+        }
+
+    }
+}
